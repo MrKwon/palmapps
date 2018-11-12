4a1e000a14bf41cbe5117cc03135afb4b8241040
MrKwon <32266963+MrKwon@users.noreply.github.com>
2018-11-13 02:10:42 +0900

    Updated 2018-11-13
    백엔드 작업 끝, 코드가 좀 찝찝한 감이 있다.

--- backend/models/index.js
+++ backend/models/index.js
@@ -15,6 +15,9 @@ db.store_infos = require('./store')(sequelize, Sequelize);
 db.orderlists = require('./orderlist')(sequelize, Sequelize);
 db.menupans = require('./menu')(sequelize, Sequelize);
 
+// 아래의 모든 릴레이션들에 _id를 붙여야 할 것 같음
+// 가독성이 너무 떨어짐
+
 // orderlist 에는 orderer column에 member의 id 가 foreignKey로 등록됨
 db.member_infos.hasMany(db.orderlists, { foreignKey: 'orderer', sourceKey: 'id' });
 db.orderlists.belongsTo(db.member_infos, { foreignKey: 'orderer', targetKey: 'id' });
@@ -28,7 +31,7 @@ db.store_infos.hasMany(db.menupans, { foreignKey: 'store_name', sourceKey: 'id' 
 db.menupans.belongsTo(db.store_infos, { foreignKey: 'store_name', targetKey: 'id' });
 
 // menupans id 하나가 orderlist에 여러 개의 order_name 들을 가지고 있음
-db.menupans.hasMany(db.orderlists, { foreignKey: 'menu', sourceKey: 'id' });
-db.orderlists.belongsTo(db.menupans, { foreignKey: 'menu', sourceKey: 'id' });
+db.menupans.hasMany(db.orderlists, { foreignKey: 'menu_id', sourceKey: 'id' });
+db.orderlists.belongsTo(db.menupans, { foreignKey: 'menu_id', sourceKey: 'id' });
 
 module.exports = db;
--- backend/routes/order.js
+++ backend/routes/order.js
@@ -8,7 +8,7 @@ const router = express.Router();
 // 주문을 등록하는 메서드
 router.post('/send', async(req, res, next) => {
 
-  const orderArray = req.body;
+  const order_array = req.body;
 
   console.log(req.body);
   console.log(req.body.length);
@@ -17,14 +17,14 @@ router.post('/send', async(req, res, next) => {
     const userinfo = await member_infos.find({
       attributes: ['id'],
       where: {
-        email: orderArray[0].email
+        email: order_array[0].email
       }
     });
 
     const username_id = userinfo.id;
 
-    for ( let i = 0; i < orderArray.length; i++) {
-      const { order_name, order_count, orderee, paytype } = orderArray[i];
+    for ( let i = 0; i < order_array.length; i++) {
+      const { order_name, order_count, orderee, paytype } = order_array[i];
 
       const menu_id = await menupans.findOne({
         attributes: ['id'],
@@ -35,7 +35,7 @@ router.post('/send', async(req, res, next) => {
       console.log(menu_id.id);
 
       await orderlists.create({
-        menu: menu_id.id,
+        menu_id: menu_id.id,
         count: order_count,
         orderer: username_id,
         paytype,
@@ -57,10 +57,12 @@ router.post('/send', async(req, res, next) => {
 
 // 주문 내역을 가져오는 메서드
 router.post('/noworderlist', async(req, res, next) => {
+  // 요청자의 이메일 주소를 받아와서
   const req_email = req.body.email;
   console.log(req_email);
 
   try {
+    // 요청자의 이메일 주소에 해당하는 id 를 찾고
     const req_id_json = await member_infos.findOne({
       attributes: ['id'],
       where: {
@@ -69,22 +71,69 @@ router.post('/noworderlist', async(req, res, next) => {
     });
     console.log(req_id_json.id);
 
-    const req_id = req_id_json.id;
+    const reqester_id = req_id_json.id; // orderer
 
-    const req_orderlist = await orderlists.findAll({
-      attributes: [''],
+    // 해당하는 id 가 orderer에 값으로 들어있는 모든 row 중에서
+    // complete 속성이 0인 모든 row 들을 orderee orderer, menu, count, createdAt 을
+    // JsonArray 형식으로 불러옴 - 여러개이기 때문에
+    const user_orderlist = await orderlists.findAll({
+      attributes: ['orderee', 'createdAt', 'menu_id', 'count' ],
       where: {
-        orderer: req_id,
+        orderer: reqester_id,
         complete: { [Op.eq]: 0 },
       }
     });
-    console.log(req_orderlist);
 
-    console.log(req_orderlist.body);
-    console.log(req_orderlist.params);
+    const tmp_json_array = JSON.parse(JSON.stringify(user_orderlist));
 
-    const json = JSON.stringify(req_orderlist);
-    return res.end();
+    // 위에서 만든 JsonArray를 프론트에 보낼 배열의 형식에 맞게 값들을 찾아 재구성
+    // ordered_store_name: store_infos.find({ attributes: ['store_name'], where: { id: orderee } });
+    // ordered_time : createdAt
+    // ordered_menu_name : menupans.find({ attributes: ['menu_name'], where: { id: menu_id } });
+    // ordered_count : count
+    // ordered_price : menupans.find({ attributes: ['menu_price'], where: { id: menu_id } });
+    //                  * count
+    let result_json_array = [ ];
+    for(let i = 0; i < tmp_json_array.length; i++ ) {
+      console.log(">>>>>>>>>>>>>", i);
+      const { orderee, createdAt, menu_id, count } = tmp_json_array[i];
+      const tmp_store_name = await store_infos.findOne({
+        attributes: ['store_name'],
+        where: {
+          id: orderee
+        }
+      })
+      const ordered_store_name = JSON.parse(JSON.stringify(tmp_store_name)).store_name;
+      console.log(ordered_store_name);
+
+      const tmp_menu_info = await menupans.findOne({
+        attributes: ['menu_name', 'menu_price'],
+        where: {
+          id: menu_id
+        }
+      });
+      const ordered_menu_name = JSON.parse(JSON.stringify(tmp_menu_info)).menu_name;
+      const ordered_menu_price = JSON.parse(JSON.stringify(tmp_menu_info)).menu_price;
+
+      const to_result_json = {
+        ordered_store_name,
+        ordered_time: createdAt,
+        ordered_menu_name,
+        ordered_count: count,
+        ordered_price: ordered_menu_price * count,
+      }
+
+      console.log(to_result_json);
+
+      result_json_array.push(to_result_json);
+    }
+
+    const json = JSON.stringify(result_json_array);
+    // console.log(reqester_orderlist.body);
+    // console.log(reqester_orderlist.params);
+
+    // const json = JSON.stringify(req_orderlist);
+    return res.end(json);
 
   } catch(error) {
     console.error(error);
@@ -92,8 +141,89 @@ router.post('/noworderlist', async(req, res, next) => {
   }
 });
 
-// router.get('/pastorderlist', async(req, res, next) => {
-//
-// });
+router.post('/pastorderlist', async(req, res, next) => {
+  // 요청자의 이메일 주소를 받아와서
+  const req_email = req.body.email;
+  console.log(req_email);
+
+  try {
+    // 요청자의 이메일 주소에 해당하는 id 를 찾고
+    const req_id_json = await member_infos.findOne({
+      attributes: ['id'],
+      where: {
+        email: req_email,
+      }
+    });
+    console.log(req_id_json.id);
+
+    const reqester_id = req_id_json.id; // orderer
+
+    // 해당하는 id 가 orderer에 값으로 들어있는 모든 row 중에서
+    // complete 속성이 0인 모든 row 들을 orderee orderer, menu, count, createdAt 을
+    // JsonArray 형식으로 불러옴 - 여러개이기 때문에
+    const user_orderlist = await orderlists.findAll({
+      attributes: ['orderee', 'createdAt', 'menu_id', 'count' ],
+      where: {
+        orderer: reqester_id,
+        complete: { [Op.eq]: 1 },
+      }
+    });
+
+    const tmp_json_array = JSON.parse(JSON.stringify(user_orderlist));
+
+    // 위에서 만든 JsonArray를 프론트에 보낼 배열의 형식에 맞게 값들을 찾아 재구성
+    // ordered_store_name: store_infos.find({ attributes: ['store_name'], where: { id: orderee } });
+    // ordered_time : createdAt
+    // ordered_menu_name : menupans.find({ attributes: ['menu_name'], where: { id: menu_id } });
+    // ordered_count : count
+    // ordered_price : menupans.find({ attributes: ['menu_price'], where: { id: menu_id } });
+    //                  * count
+    let result_json_array = [ ];
+    for(let i = 0; i < tmp_json_array.length; i++ ) {
+      console.log(">>>>>>>>>>>>>", i);
+      const { orderee, createdAt, menu_id, count } = tmp_json_array[i];
+      const tmp_store_name = await store_infos.findOne({
+        attributes: ['store_name'],
+        where: {
+          id: orderee
+        }
+      })
+      const ordered_store_name = JSON.parse(JSON.stringify(tmp_store_name)).store_name;
+      console.log(ordered_store_name);
+
+      const tmp_menu_info = await menupans.findOne({
+        attributes: ['menu_name', 'menu_price'],
+        where: {
+          id: menu_id
+        }
+      });
+      const ordered_menu_name = JSON.parse(JSON.stringify(tmp_menu_info)).menu_name;
+      const ordered_menu_price = JSON.parse(JSON.stringify(tmp_menu_info)).menu_price;
+
+      const to_result_json = {
+        ordered_store_name,
+        ordered_time: createdAt,
+        ordered_menu_name,
+        ordered_count: count,
+        ordered_price: ordered_menu_price * count,
+      }
+
+      console.log(to_result_json);
+
+      result_json_array.push(to_result_json);
+    }
+
+    const json = JSON.stringify(result_json_array);
+    // console.log(reqester_orderlist.body);
+    // console.log(reqester_orderlist.params);
+
+    // const json = JSON.stringify(req_orderlist);
+    return res.end(json);
+
+  } catch(error) {
+    console.error(error);
+    next(error);
+  }
+});
 
 module.exports = router;
